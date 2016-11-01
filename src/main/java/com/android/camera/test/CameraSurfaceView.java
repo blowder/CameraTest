952ec11d8f895078d1957750a9952dd31970f391
sesshoumaru <blowde@mail.ru>
2016-11-02 01:42:10 +0200

    preview stream analyze POC

--- src/main/java/com/android/camera/test/CameraSurfaceView.java
+++ src/main/java/com/android/camera/test/CameraSurfaceView.java
@@ -1,18 +1,22 @@
 package com.android.camera.test;
 
 import android.content.Context;
+import android.graphics.*;
 import android.hardware.Camera;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
     private static String TAG = CameraSurfaceView.class.getSimpleName();
     private Camera camera;
+    private ImageView target;
 
     public CameraSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,12 +29,12 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
     }
 
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+    public void surfaceChanged(SurfaceHolder holder, int format, final int w, final int h) {
         Log.e(TAG, "surfaceChanged() holder width=" + w + ", height=" + h);
         Camera.Size cameraSize = getOptimalPreviewSize(camera.getParameters().getSupportedPreviewSizes(), w, h);
         Log.e(TAG, "surfaceChanged() camera width=" + cameraSize.width + ", height=" + cameraSize.height);
         try {
-            Camera.Parameters parameters = camera.getParameters();
+            final Camera.Parameters parameters = camera.getParameters();
             parameters.setPreviewSize(cameraSize.width, cameraSize.height);
             camera.setParameters(parameters);
             if (w < h)
@@ -38,6 +42,26 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
             camera.setPreviewDisplay(holder);
             camera.setPreviewCallback(new Camera.PreviewCallback() {
                 public void onPreviewFrame(byte[] data, Camera camera) {
+                    if (target != null) {
+                        Camera.Parameters parameters = camera.getParameters();
+                        int width = parameters.getPreviewSize().width;
+                        int height = parameters.getPreviewSize().height;
+
+                        YuvImage yuv = new YuvImage(data, parameters.getPreviewFormat(), width, height, null);
+
+                        ByteArrayOutputStream out = new ByteArrayOutputStream();
+                        yuv.compressToJpeg(new Rect(0, 0, width, height), 50, out);
+
+                        byte[] bytes = out.toByteArray();
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        if (w < h) {
+                            Matrix matrix = new Matrix();
+                            matrix.postRotate(90);
+                            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                        }
+
+                        target.setImageBitmap(bitmap);
+                    }
                     Log.e(TAG, "onPreviewFrame(): data=" + data.length);
                 }
             });
@@ -82,5 +106,9 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
             }
         }
         return optimalSize;
+    }
+
+    public void setTarget(ImageView target) {
+        this.target = target;
     }
 }
--- src/main/java/com/android/camera/test/MainActivity.java
+++ src/main/java/com/android/camera/test/MainActivity.java
@@ -2,6 +2,9 @@ package com.android.camera.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.widget.ImageView;
 import com.android.camera.test.R;
 
 
@@ -9,5 +12,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.main);
+        CameraSurfaceView view = (CameraSurfaceView) findViewById(R.id.surface);
+        ImageView image = (ImageView) findViewById(R.id.image);
+        view.setTarget(image);
     }
 }
