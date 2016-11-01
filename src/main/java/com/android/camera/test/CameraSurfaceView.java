5127639bbf2ab861eb66c15a308b425d4f5b4233
sesshoumaru <blowde@mail.ru>
2016-11-02 01:18:51 +0200

    preview stretch problem was resolved

--- src/main/java/com/android/camera/test/CameraSurfaceView.java
+++ src/main/java/com/android/camera/test/CameraSurfaceView.java
@@ -1,52 +1,86 @@
 package com.android.camera.test;
-import android.view.SurfaceView;
-import android.view.SurfaceHolder;
-import android.view.SurfaceHolder.Callback;
+
 import android.content.Context;
 import android.hardware.Camera;
-import android.hardware.Camera.PreviewCallback;
-import java.io.IOException;
-import android.util.Log;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
-	public SurfaceHolder mHolder;
-	public Camera camera;
+import java.io.IOException;
+import java.util.List;
 
-	public CameraSurfaceView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		mHolder = getHolder();
-		mHolder.addCallback(this);
-		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
+public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    private static String TAG = CameraSurfaceView.class.getSimpleName();
+    private Camera camera;
 
-	@Override
-	public void surfaceCreated(final SurfaceHolder holder) {
-		camera = Camera.open();
-		try {
-			camera.setPreviewDisplay(holder);
-			camera.startPreview();
-			camera.setPreviewCallback(new PreviewCallback() {
-				public void onPreviewFrame(byte[] data, Camera arg1) {
-					Log.e("Camera","Bytes received" + data.length);
-				}
-			});
-		} catch (IOException e) {}
-	}
+    public CameraSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        getHolder().addCallback(this);
+    }
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-		try {
-			camera.stopPreview();
-			camera.setPreviewDisplay(holder);
-			camera.startPreview();
-		} catch (IOException e) {}
-	}
+    public void surfaceCreated(final SurfaceHolder holder) {
+        Log.e(TAG, "surfaceCreated()");
+        if (this.camera == null) this.camera = Camera.open();
+    }
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder){
-		camera.stopPreview();
-		camera.release();
-	}
 
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        Log.e(TAG, "surfaceChanged() holder width=" + w + ", height=" + h);
+        Camera.Size cameraSize = getOptimalPreviewSize(camera.getParameters().getSupportedPreviewSizes(), w, h);
+        Log.e(TAG, "surfaceChanged() camera width=" + cameraSize.width + ", height=" + cameraSize.height);
+        try {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewSize(cameraSize.width, cameraSize.height);
+            camera.setParameters(parameters);
+            if (w < h)
+                camera.setDisplayOrientation(90);
+            camera.setPreviewDisplay(holder);
+            camera.setPreviewCallback(new Camera.PreviewCallback() {
+                public void onPreviewFrame(byte[] data, Camera camera) {
+                    Log.e(TAG, "onPreviewFrame(): data=" + data.length);
+                }
+            });
+            camera.startPreview();
+        } catch (IOException ignored) {
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.e(TAG, "surfaceDestroyed();");
+        camera.stopPreview();
+        camera.setPreviewCallback(null);
+        camera.release();
+        camera = null;
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - h) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - h);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - h) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - h);
+                }
+            }
+        }
+        return optimalSize;
+    }
 }
